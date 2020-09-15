5c1b107de745d6931091181a292bc786086f4acc
WeizhengJ <70171753+WeizhengJ@users.noreply.github.com>
2020-09-15 14:07:20 -0500

    Update rgi.h

--- rgi.h
+++ rgi.h
@@ -8,29 +8,38 @@
 #include <iomanip>
 #include <cstdlib>
 
-// You need to implement these two functions ...
-// void genRndNums(vector<int> &v);
-// void printVec(const vector<int> &v);
+/* Weizheng Jiang
+   z1861817
+   CSCI 340 section 2
+   
+   
+   I certify that this is my own work and where appropriate an extension 
+   of the starter code provided for the assignment.
+*/
 
 using namespace std;
 
+//The parameter is the reference of vector v
+
 void genRndNums(vector<int> &v)
 {
-	int SEED = 1;
-	int HIGH = 10000;
+	int SEED = 1;     //Declare the seed to be 1
+	int HIGH = 10000; //Declare the range of the number that will be generated
 	int LOW = 1;
-	srand(SEED);
+	srand(SEED);      //Generate the number
 	for(auto i = v.begin(); i != v.end(); ++i)
-		(*i) = (rand() % HIGH) + LOW;
+		(*i) = (rand() % HIGH) + LOW;     //Store the number in the vector
 }
+
+//The parameter is the reference of vector
 
 void printVec(const vector<int> &v)
 {
 	int NO_ITEMS = 12;
 	int ITEM_W = 5;
-	for(size_t i = 0; i < v.size(); ++i)
+	for(size_t i = 0; i < v.size(); ++i) 
 	{
-		if(i % NO_ITEMS == 0)
+		if(i % NO_ITEMS == 0) //print exact 12 numbers per line
 			cout << endl;
 
 		cout << setw(ITEM_W) << v.at(i);
