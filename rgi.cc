d4ec64acccb6f3e31b43cf0f8bfce194efd0f6d6
WeizhengJ <70171753+WeizhengJ@users.noreply.github.com>
2020-09-15 13:52:58 -0500

    Update rgi.cc

--- rgi.cc
+++ rgi.cc
@@ -2,12 +2,20 @@
 #include <vector>
 #include <algorithm>
 
-// Add needed constants
-
-// Add code for genRndNums
-// Add code for printVec
+/* Weizheng Jiang
+   z1861817
+   CSCI 340 section 2
+   
+   I certify that this is my own work and where appropriate an extension 
+   of the starter code provided for the assignment.
+   
+*/
 
 using namespace std;
+
+//genRndNums will generate the random numbers and use the push_back method store them in vector v
+//Then use the sort method to sort the elements in the range [begin,end)
+//Then call the printVec function to print the sorted element
 
 int main()
 {
